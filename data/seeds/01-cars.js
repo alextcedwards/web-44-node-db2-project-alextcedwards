0653aa7c8e25acc5bff4507d029feaffbd1f1a29
Alex Edwards <alextcedwards@gmail.com>
2021-08-10 22:45:26 -0600

    mvp done baby

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
-// STRETCH
+const cars = [
+  {
+    vin: "JN1CV6EL1BM261706",
+    make: "honda",
+    model: "civic",
+    mileage: 30000,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "1FAHP33N48W139067",
+    make: "honda",
+    model: "prelude",
+    mileage: 70000,
+    title: "salvage",
+    transmission: "manual",
+  },
+  {
+    vin: "1G8AJ52FX4Z101610",
+    make: "honda",
+    model: "accord",
+    mileage: 85000,
+    title: "clean",
+    transmission: "automatic",
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
